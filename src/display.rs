8b16f516c63c28a661938d72b9dd64afcb5e1238
Harry Yu <harryyunull@gmail.com>
2022-10-11 12:41:01 -0400

    refactoring

---
+++ src/display.rs
@@ -0,0 +1,65 @@
+use windows::Graphics::Capture::GraphicsCaptureItem;
+use windows::Win32::Foundation::{BOOL, LPARAM, RECT};
+use windows::Win32::Graphics::Gdi::{
+    EnumDisplayMonitors, GetMonitorInfoW, HDC, HMONITOR, MONITORINFOEXW,
+};
+use windows::Win32::System::WinRT::Graphics::Capture::IGraphicsCaptureItemInterop;
+
+use crate::result::Result;
+
+#[derive(Clone)]
+pub struct DisplayInfo {
+    pub handle: HMONITOR,
+    pub display_name: String,
+    pub size: (u32, u32),
+}
+
+impl DisplayInfo {
+    pub fn displays() -> Result<Vec<Self>> {
+        unsafe {
+            let displays = Box::into_raw(Box::new(Vec::<DisplayInfo>::new()));
+            EnumDisplayMonitors(
+                HDC(0),
+                None,
+                Some(enum_monitor),
+                LPARAM(displays as isize),
+            );
+            Ok(*Box::from_raw(displays))
+        }
+    }
+
+    pub fn new(monitor_handle: HMONITOR) -> Result<Self> {
+        let mut info = MONITORINFOEXW::default();
+        info.monitorInfo.cbSize = std::mem::size_of::<MONITORINFOEXW>() as u32;
+
+        unsafe {
+            GetMonitorInfoW(monitor_handle, &mut info as *mut _ as *mut _).ok()?;
+        }
+
+        let display_name = String::from_utf16_lossy(&info.szDevice)
+            .trim_matches(char::from(0))
+            .to_string();
+
+        Ok(Self {
+            handle: monitor_handle,
+            display_name,
+            size: ((info.monitorInfo.rcMonitor.right - info.monitorInfo.rcMonitor.left) as u32,
+                   (info.monitorInfo.rcMonitor.bottom - info.monitorInfo.rcMonitor.top) as u32),
+        })
+    }
+
+    pub fn create_capture_item_for_monitor(&self) -> Result<GraphicsCaptureItem> {
+        let interop = windows::core::factory::<GraphicsCaptureItem, IGraphicsCaptureItemInterop>()?;
+        Ok(unsafe { interop.CreateForMonitor(self.handle) }?)
+    }
+}
+
+// callback function for EnumDisplayMonitors
+extern "system" fn enum_monitor(monitor: HMONITOR, _: HDC, _: *mut RECT, state: LPARAM) -> BOOL {
+    unsafe {
+        // get the vector from the param, use leak because this function is not responsible for its lifetime
+        let state = Box::leak(Box::from_raw(state.0 as *mut Vec<DisplayInfo>));
+        state.push(DisplayInfo::new(monitor).unwrap());
+    }
+    true.into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,34 @@
-mod d3d;
-
+use std::{mem, slice};
 use std::ffi::c_uchar;
 use std::fs::File;
 use std::io::Write;
-use std::{mem, slice};
 use std::process::{Command, Stdio};
 use std::ptr::null_mut;
 use std::sync::mpsc::channel;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+
 use openh264::formats::{RBGYUVConverter, YUVSource};
 use openh264_sys2::{ENCODER_OPTION_DATAFORMAT, SFrameBSInfo, SSourcePicture, videoFormatBGR, videoFormatI420};
-use windows::core::{IInspectable, Interface, Result};
-use windows::Graphics::Capture::{Direct3D11CaptureFramePool, GraphicsCaptureItem};
-use windows::Win32::Foundation::{BOOL, LPARAM, RECT};
-use windows::Graphics::DirectX::DirectXPixelFormat;
+use windows::core::{IInspectable, Interface};
 use windows::Foundation::TypedEventHandler;
+use windows::Graphics::Capture::{Direct3D11CaptureFramePool, GraphicsCaptureItem};
+use windows::Graphics::DirectX::DirectXPixelFormat;
 use windows::Win32::Graphics::Direct3D11::{D3D11_BIND_FLAG, D3D11_CPU_ACCESS_READ, D3D11_MAP_READ, D3D11_RESOURCE_MISC_FLAG, D3D11_TEXTURE2D_DESC, D3D11_USAGE_STAGING, ID3D11Resource, ID3D11Texture2D};
-use windows::Win32::Graphics::Gdi::{
-    EnumDisplayMonitors, GetMonitorInfoW, HDC, HMONITOR, MONITORINFOEXW,
-};
 use windows::Win32::System::WinRT::{
-    Graphics::Capture::IGraphicsCaptureItemInterop, RoInitialize, RO_INIT_MULTITHREADED,
+    Graphics::Capture::IGraphicsCaptureItemInterop, RO_INIT_MULTITHREADED, RoInitialize,
 };
 use x264_sys::{
-    X264_CSP_BGRA, x264_nal_t,x264_picture_alloc, x264_picture_t , x264_param_default_preset,
-    x264_encoder_open, x264_encoder_close, x264_param_apply_profile, x264_encoder_encode
+    X264_CSP_BGRA, x264_encoder_close, x264_encoder_encode, x264_encoder_open, x264_nal_t,
+    x264_param_apply_profile, x264_param_default_preset, x264_picture_alloc, x264_picture_t,
 };
 
-#[derive(Clone)]
-pub struct DisplayInfo {
-    pub handle: HMONITOR,
-    pub display_name: String,
-}
+use crate::display::DisplayInfo;
+use crate::result::Result;
 
-impl DisplayInfo {
-    pub fn new(monitor_handle: HMONITOR) -> Result<Self> {
-        let mut info = MONITORINFOEXW::default();
-        info.monitorInfo.cbSize = std::mem::size_of::<MONITORINFOEXW>() as u32;
-
-        unsafe {
-            GetMonitorInfoW(monitor_handle, &mut info as *mut _ as *mut _).ok()?;
-        }
-
-        let display_name = String::from_utf16_lossy(&info.szDevice)
-            .trim_matches(char::from(0))
-            .to_string();
-
-        Ok(Self {
-            handle: monitor_handle,
-            display_name,
-        })
-    }
-}
-
-fn create_capture_item_for_monitor(monitor_handle: HMONITOR) -> Result<GraphicsCaptureItem> {
-    let interop = windows::core::factory::<GraphicsCaptureItem, IGraphicsCaptureItemInterop>()?;
-    unsafe { interop.CreateForMonitor(monitor_handle) }
-}
+mod d3d;
+mod display;
+mod result;
 
 unsafe fn take_screenshot(item: &GraphicsCaptureItem) -> Result<()> {
     let item_size = item.Size()?;
@@ -89,17 +60,18 @@ unsafe fn take_screenshot(item: &GraphicsCaptureItem) -> Result<()> {
     x264_param_default_preset(
         par.as_mut_ptr(),
         b"ultrafast\0".as_ptr() as *const i8,
-        b"zerolatency\0".as_ptr() as *const i8
+        b"zerolatency\0".as_ptr() as *const i8,
     );
     let mut par = par.assume_init();
     par.i_width = 3840;
     par.i_height = 2160;
     par.i_fps_num = 60;
+    par.i_threads = 4;
     par.b_annexb = true as i32;
 
     par.i_csp = X264_CSP_BGRA as i32;
-    let mut pic_in : x264_picture_t = mem::MaybeUninit::uninit().assume_init();
-    x264_picture_alloc( &mut pic_in, par.i_csp, par.i_width, par.i_height );
+    let mut pic_in: x264_picture_t = mem::MaybeUninit::uninit().assume_init();
+    x264_picture_alloc(&mut pic_in, par.i_csp, par.i_width, par.i_height);
     let x = x264_encoder_open(&mut par);
 
     let mut nal: *mut x264_nal_t = mem::MaybeUninit::uninit().assume_init();
@@ -143,13 +115,13 @@ unsafe fn take_screenshot(item: &GraphicsCaptureItem) -> Result<()> {
             mapped.pData as *const _,
             (desc.Height * mapped.RowPitch) as usize,
         );
-        pic_in.img.plane=[
+        pic_in.img.plane = [
             (slice.as_ptr() as *mut u8).add(0),
             null_mut(),
             null_mut(),
             null_mut(),
         ];
-        pic_in.i_pts = ((frame_ms - start_relative_time.unwrap()) as f64 / (1.0/60.0*1000.0)).round() as i64;
+        pic_in.i_pts = ((frame_ms - start_relative_time.unwrap()) as f64 / (1.0 / 60.0 * 1000.0)).round() as i64;
         let frame_size = x264_encoder_encode(x, &mut nal as *mut *mut _, &mut nal_size, &mut pic_in, &mut pic_out);
         d3d_context.Unmap(&resource, 0);
         output.write_all(slice::from_raw_parts((*nal).p_payload, frame_size as usize)).expect("TODO: panic message");
@@ -157,7 +129,7 @@ unsafe fn take_screenshot(item: &GraphicsCaptureItem) -> Result<()> {
         encoding_time.push(timer.elapsed().as_millis());
         counter += 1;
         if counter % 100 == 0 {
-            let expected_frames = (start.elapsed().as_millis() as f64)/(1.0/60.0*1000.0);
+            let expected_frames = (start.elapsed().as_millis() as f64) / (1.0 / 60.0 * 1000.0);
             println!("fps: {}", counter as f64 / start.elapsed().as_secs_f64());
             println!("loss: {} ({}%)", expected_frames - counter as f64, (expected_frames - counter as f64) / expected_frames * 100.0);
             println!("time elapsed: {:?}", start.elapsed());
@@ -181,32 +153,12 @@ fn main() -> Result<()> {
     let (w, h) = (d.width(), d.height());
     println!("{}x{} screen", w, h);
 
-    let displays = unsafe {
-        let displays = Box::into_raw(Box::new(Vec::<DisplayInfo>::new()));
-        EnumDisplayMonitors(
-            HDC(0),
-            None,
-            Some(enum_monitor),
-            LPARAM(displays as isize),
-        );
-        Box::from_raw(displays)
-    };
+    let displays = DisplayInfo::displays()?;
     for display in displays.iter() {
         println!("Display: {} {}", display.display_name, display.handle.0);
     }
-    let item = create_capture_item_for_monitor(displays[1].handle)?;
+    let item = displays[1].create_capture_item_for_monitor()?;
     println!("Item: {:?}", item);
-    unsafe{take_screenshot(&item)?;}
+    unsafe { take_screenshot(&item)?; }
     Ok(())
-}
-
-// callback function for EnumDisplayMonitors
-extern "system" fn enum_monitor(monitor: HMONITOR, _: HDC, _: *mut RECT, state: LPARAM) -> BOOL {
-    unsafe {
-        // get the vector from the param, use leak because this function is not responsible for its lifetime
-        let state = Box::leak(Box::from_raw(state.0 as *mut Vec<DisplayInfo>));
-        let display_info = DisplayInfo::new(monitor).unwrap();
-        state.push(display_info);
-    }
-    true.into()
 }
