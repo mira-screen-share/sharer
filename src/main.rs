f9466ab3b431d4e4d4c58d129d4b947f9a00b669
Harry Yu <harryyunull@gmail.com>
2022-10-14 19:09:24 -0400

    add params

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::result::Result;
 use crate::capture::ScreenCapture;
 use crate::output::OutputSink;
 use crate::encoder::Encoder;
+use clap::Parser;
 
 mod d3d;
 mod display;
@@ -11,12 +12,28 @@ mod capture;
 mod encoder;
 mod output;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Display index
+    #[arg(short, long, default_value = "0")]
+    display: usize,
+}
+
 fn main() -> Result<()> {
+    let args = Args::parse();
+
     let displays = DisplayInfo::displays()?;
-    for display in displays.iter() {
-        println!("Display: {} {}x{}", display.display_name, display.resolution.0, display.resolution.1);
+    for (i, display) in displays.iter().enumerate() {
+        println!(
+            "display: {} {}x{} {}",
+            display.display_name,
+            display.resolution.0,
+            display.resolution.1,
+            if i==args.display { "(selected)" } else { "" },
+        );
     }
-    let display = displays.iter().nth(1).unwrap();
+    let display = displays.iter().nth(args.display).unwrap();
     let item = display.create_capture_item_for_monitor()?;
     let mut capture = capture::WGCScreenCapture::new(&item)?;
     let mut encoder = encoder::X264Encoder::new(display.resolution.0, display.resolution.1);
