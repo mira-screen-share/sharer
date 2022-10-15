4f5674a05917551c0a8aab1333ff3dcc1ee517a4
Harry Yu <harryyunull@gmail.com>
2022-10-15 15:38:39 -0400

    websocket connection

--- src/signaller.rs
+++ src/signaller.rs
@@ -1,33 +1,60 @@
 use tokio::sync::mpsc::{Sender, Receiver};
 use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 use async_trait::async_trait;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use futures_util::{future, pin_mut, StreamExt};
+use serde::{Deserialize, Serialize};
+use crate::Result;
 
 #[async_trait]
 pub trait Signaller {
     fn send(&self);
-    async fn recv_offer(&mut self) -> Option<RTCSessionDescription>;
+    async fn recv_answer(&mut self) -> Option<RTCSessionDescription>;
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "message_type", content = "payload")]
+enum SignallerMessage {
+    Answer(RTCSessionDescription),
 }
 
 pub struct WebSocketSignaller {
-    sdp_sender: Sender<RTCSessionDescription>,
     sdp_receiver: Receiver<RTCSessionDescription>,
 }
 
 impl WebSocketSignaller {
-    pub fn new(url: & str) -> Self {
-        //serde_json::from_str::<RTCSessionDescription>(&desc_data)?
+    pub async fn new(url: &str) -> Result<Self> {
         let (sdp_sender, sdp_receiver) = tokio::sync::mpsc::channel::<RTCSessionDescription>(1);
-        Self {
-            sdp_sender,
+
+        let url = url::Url::parse(&url).unwrap();
+        info!("Establishing websocket connection to {}", url);
+        let (ws_stream, _) = connect_async(url).await?;
+        debug!("Websocket connection established");
+        let (write, read) = ws_stream.split();
+        tokio::spawn(async move {
+            read.for_each(|msg| async {
+                trace!("Received websocket message: {:?}", msg);
+                let text = msg.unwrap().into_text().unwrap();
+                let msg = serde_json::from_str::<SignallerMessage>(&text).unwrap();
+                debug!("Deserialized websocket message: {:#?}", msg);
+                match msg {
+                    SignallerMessage::Answer(answer) => {
+                        sdp_sender.send(answer).await.unwrap();
+                    }
+                }
+            }).await;
+        });
+
+        Ok(Self {
             sdp_receiver,
-        }
+        })
     }
 }
 
 #[async_trait]
 impl Signaller for WebSocketSignaller {
     fn send(&self) {}
-    async fn recv_offer(&mut self) -> Option<RTCSessionDescription> {
+    async fn recv_answer(&mut self) -> Option<RTCSessionDescription> {
         self.sdp_receiver.recv().await
     }
 }
