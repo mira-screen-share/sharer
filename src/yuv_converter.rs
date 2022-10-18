3d77e9e2560a130c10288854e004aac61956a955
Harry Yu <harryyunull@gmail.com>
2022-10-18 14:05:49 -0400

    faster yuv convert 35->12ms 67% at 30FPS

--- src/yuv_converter.rs
+++ src/yuv_converter.rs
@@ -1,3 +1,5 @@
+use std::num::Wrapping;
+
 pub struct BGR0YUVConverter {
     yuv: Vec<u8>,
     width: usize,
@@ -16,57 +18,46 @@ impl BGR0YUVConverter {
 
     /// Converts the RGB array.
     pub fn convert(&mut self, rgb: &[u8]) {
-        let width = self.width;
-        let height = self.height;
+            let image_size = self.width * self.height as usize;
+            let mut upos = image_size;
+            let mut vpos = upos + upos / 4 as usize;
+            let mut i = 0 as usize;
 
-        let u_base = width * height;
-        let v_base = u_base + u_base / 4;
-        let half_width = width / 2;
+            for  line in 0..self.height {
+                if line % 2 != 0 {
+                    let mut x = 0 as usize;
+                    while x < self.width {
+                        let b = Wrapping(rgb[4 * i] as u32);
+                        let g = Wrapping(rgb[4 * i + 1] as u32);
+                        let r = Wrapping(rgb[4 * i + 2] as u32);
 
-        assert_eq!(rgb.len(), width * height * 4);
-        assert_eq!(width % 2, 0, "width needs to be multiple of 2");
-        assert_eq!(height % 2, 0, "height needs to be a multiple of 2");
+                        self.yuv[i] = (((Wrapping(66)*r + Wrapping(129)*g + Wrapping(25)*b) >> 8) + Wrapping(16)).0 as u8;
+                        self.yuv[upos] = (((Wrapping(-38i8 as u32)*r + Wrapping(-74i8 as u32)*g + Wrapping(112)*b) >> 8) + Wrapping(128)).0 as u8;
+                        self.yuv[vpos] = (((Wrapping(112)*r + Wrapping(-94i8 as u32)*g + Wrapping(-18i8 as u32)*b) >> 8) + Wrapping(128)).0 as u8;
 
-        // y is full size, u, v is quarter size
-        let pixel = |x: usize, y: usize| -> (f32, f32, f32) {
-            // two dim to single dim
-            let base_pos = (x + y * width) * 4;
-            (rgb[base_pos + 2] as f32, rgb[base_pos + 1] as f32, rgb[base_pos] as f32)
-        };
+                        i+=1;
+                        upos+=1;
+                        vpos+=1;
 
-        let write_y = |yuv: &mut [u8], x: usize, y: usize, rgb: (f32, f32, f32)| {
-            yuv[x + y * width] = (0.2578125 * rgb.0 + 0.50390625 * rgb.1 + 0.09765625 * rgb.2 + 16.0) as u8;
-        };
+                        let b = Wrapping(rgb[4 * i] as u32);
+                        let g = Wrapping(rgb[4 * i + 1] as u32);
+                        let r = Wrapping(rgb[4 * i + 2] as u32);
 
-        let write_u = |yuv: &mut [u8], x: usize, y: usize, rgb: (f32, f32, f32)| {
-            yuv[u_base + x + y * half_width] = (-0.1484375 * rgb.0 + -0.2890625 * rgb.1 + 0.4375 * rgb.2 + 128.0) as u8;
-        };
+                        self.yuv[i] = (((Wrapping(66)*r + Wrapping(129)*g + Wrapping(25)*b) >> 8) + Wrapping(16)).0 as u8;
+                        i+=1;
+                        x+=2;
+                    }
+                } else {
+                    for x in 0..self.width{
+                        let b = Wrapping(rgb[4 * i] as u32);
+                        let g = Wrapping(rgb[4 * i + 1] as u32);
+                        let r = Wrapping(rgb[4 * i + 2] as u32);
 
-        let write_v = |yuv: &mut [u8], x: usize, y: usize, rgb: (f32, f32, f32)| {
-            yuv[v_base + x + y * half_width] = (0.4375 * rgb.0 + -0.3671875 * rgb.1 + -0.0703125 * rgb.2 + 128.0) as u8;
-        };
-
-        for i in 0..width / 2 {
-            for j in 0..height / 2 {
-                let px = i * 2;
-                let py = j * 2;
-                let pix0x0 = pixel(px, py);
-                let pix0x1 = pixel(px, py + 1);
-                let pix1x0 = pixel(px + 1, py);
-                let pix1x1 = pixel(px + 1, py + 1);
-                let avg_pix = (
-                    (pix0x0.0 as u32 + pix0x1.0 as u32 + pix1x0.0 as u32 + pix1x1.0 as u32) as f32 / 4.0,
-                    (pix0x0.1 as u32 + pix0x1.1 as u32 + pix1x0.1 as u32 + pix1x1.1 as u32) as f32 / 4.0,
-                    (pix0x0.2 as u32 + pix0x1.2 as u32 + pix1x0.2 as u32 + pix1x1.2 as u32) as f32 / 4.0,
-                );
-                write_y(&mut self.yuv[..], px, py, pix0x0);
-                write_y(&mut self.yuv[..], px, py + 1, pix0x1);
-                write_y(&mut self.yuv[..], px + 1, py, pix1x0);
-                write_y(&mut self.yuv[..], px + 1, py + 1, pix1x1);
-                write_u(&mut self.yuv[..], i, j, avg_pix);
-                write_v(&mut self.yuv[..], i, j, avg_pix);
+                        self.yuv[i] = (((Wrapping(66)*r + Wrapping(129)*g + Wrapping(25)*b) >> 8) + Wrapping(16)).0 as u8;
+                        i+=1;
+                    }
+                }
             }
-        }
     }
 
     fn width(&self) -> i32 {
@@ -90,5 +81,13 @@ impl BGR0YUVConverter {
         let base_u = self.width * self.height;
         let base_v = base_u + base_u / 4;
         &self.yuv[base_v..]
+    }
+}
+
+fn clamp(val: i32) -> u8 {
+    match val {
+        ref v if *v < 0 => 0,
+        ref v if *v > 255 => 255,
+        v => v as u8,
     }
 }
