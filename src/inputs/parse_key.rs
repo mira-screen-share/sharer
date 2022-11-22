0757fa248484d94cab4be781758837dc4db9d079
Harry Yu <harryyunull@gmail.com>
2022-11-22 18:10:30 -0500

    change to keyboardevent.code

--- src/inputs/parse_key.rs
+++ src/inputs/parse_key.rs
@@ -2,6 +2,7 @@ use crate::Result;
 use failure::format_err;
 
 pub trait FromJsKey {
+    /// convert from KeyboardEvent.code
     fn from_js_key(s: &str) -> Result<Self>
     where
         Self: Sized;
@@ -9,17 +10,41 @@ pub trait FromJsKey {
 
 impl FromJsKey for enigo::Key {
     fn from_js_key(s: &str) -> Result<Self> {
-        if s.len() == 1 {
-            return Ok(enigo::Key::Layout(s.chars().next().unwrap()));
+        // Handle Digit0-9
+        if s.starts_with("Digit") ||
+            (s.len() == 7 && s.starts_with("Numpad")) {
+            let digit = s.chars().last()?;
+            return Ok(enigo::Key::Layout(digit));
         }
 
+        // Handle KeyA-Z
+        if s.starts_with("Key") {
+            let letter = s.chars().last()?.to_ascii_lowercase();
+            return Ok(enigo::Key::Layout(letter));
+        }
+
+        // Unsupported Keys:
+        // Pause, NumLock, Context Menu, Numpad*
+        // Currently does not distinguish between left and right keys
         match s {
-            "Alt" => Ok(enigo::Key::Alt),
             "Backspace" => Ok(enigo::Key::Backspace),
-            "Control" => Ok(enigo::Key::Control),
-            "Delete" => Ok(enigo::Key::Delete),
-            "End" => Ok(enigo::Key::End),
+            "Tab" => Ok(enigo::Key::Tab),
+            "ShiftLeft" => Ok(enigo::Key::Shift),
+            "ShiftRight" => Ok(enigo::Key::Shift),
+            "ControlLeft" => Ok(enigo::Key::Control),
+            "ControlRight" => Ok(enigo::Key::Control),
+            "AltLeft" => Ok(enigo::Key::Alt),
+            "AltRight" => Ok(enigo::Key::Alt),
+            "CapsLock" => Ok(enigo::Key::CapsLock),
             "Escape" => Ok(enigo::Key::Escape),
+            "Space" => Ok(enigo::Key::Space),
+            "ArrowLeft" => Ok(enigo::Key::LeftArrow),
+            "ArrowUp" => Ok(enigo::Key::UpArrow),
+            "ArrowRight" => Ok(enigo::Key::RightArrow),
+            "ArrowDown" => Ok(enigo::Key::DownArrow),
+            "MetaLeft" => Ok(enigo::Key::Meta),
+            "MetaRight" => Ok(enigo::Key::Meta),
+            "End" => Ok(enigo::Key::End),
             "F1" => Ok(enigo::Key::F1),
             "F2" => Ok(enigo::Key::F2),
             "F3" => Ok(enigo::Key::F3),
@@ -32,17 +57,20 @@ impl FromJsKey for enigo::Key {
             "F10" => Ok(enigo::Key::F10),
             "F11" => Ok(enigo::Key::F11),
             "F12" => Ok(enigo::Key::F12),
+            "BracketLeft" => Ok(enigo::Key::Layout('[')),
+            "BracketRight" => Ok(enigo::Key::Layout(']')),
+            "Backslash" => Ok(enigo::Key::Layout('\\')),
+            "Semicolon" => Ok(enigo::Key::Layout(';')),
+            "Backquote" => Ok(enigo::Key::Layout('`')),
+            "Quote" => Ok(enigo::Key::Layout('\'')),
+            "Equal" => Ok(enigo::Key::Layout('=')),
+            "Minus" => Ok(enigo::Key::Layout('-')),
+            "Comma" => Ok(enigo::Key::Layout(',')),
+            "Period" => Ok(enigo::Key::Layout('.')),
+            "Slash" => Ok(enigo::Key::Layout('/')),
+            "Delete" => Ok(enigo::Key::Delete),
             "Home" => Ok(enigo::Key::Home),
-            "ArrowLeft" => Ok(enigo::Key::LeftArrow),
-            "ArrowUp" => Ok(enigo::Key::UpArrow),
-            "ArrowRight" => Ok(enigo::Key::RightArrow),
-            "ArrowDown" => Ok(enigo::Key::DownArrow),
-            "Meta" => Ok(enigo::Key::Meta),
-            "Tab" => Ok(enigo::Key::Tab),
             "Enter" => Ok(enigo::Key::Return),
-            "Shift" => Ok(enigo::Key::Shift),
-            "CapsLock" => Ok(enigo::Key::CapsLock),
-            "Space" => Ok(enigo::Key::Space),
             "PageUp" => Ok(enigo::Key::PageUp),
             "PageDown" => Ok(enigo::Key::PageDown),
             _ => Err(format_err!("Unknown key: {}", s)),
