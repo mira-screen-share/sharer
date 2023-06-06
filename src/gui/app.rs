1cb7edf3ba6d2066f889de354eaad9d0b1bd37a8
Mark Wang <carbonylgp@gmail.com>
2023-06-06 15:45:50 -0400

    copy link

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,16 +1,15 @@
 use std::path::Path;
 
 use clap::Parser;
-use iced::{
-    Application, Command, Element, executor, Theme,
-};
+use clipboard::{ClipboardContext, ClipboardProvider};
+use iced::{Application, Background, Color, Command, Element, executor, Theme};
+use iced::theme::{Button, Palette};
 use iced::widget::{button, row, text, text_input};
-use tokio_util::sync::CancellationToken;
+use iced::widget::button::Appearance;
 
 use crate::capture::capturer;
 use crate::capture::capturer::Capturer;
 use crate::config;
-use crate::config::Config;
 use crate::gui::macros::column_iced;
 
 #[derive(Clone, Debug)]
@@ -19,6 +18,7 @@ pub enum Message {
     Stop,
     SetMaxFps(String),
     SetDisplay(String),
+    CopyInviteLink,
 }
 
 pub struct App {
@@ -65,6 +65,12 @@ impl Application for App {
                     self.capturer.args.display = value;
                 }
             }
+            Message::CopyInviteLink => {
+                if let Some(invite_link) = self.capturer.get_invite_link() {
+                    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+                    ctx.set_contents(invite_link).unwrap();
+                }
+            }
         }
 
         Command::none()
@@ -73,43 +79,90 @@ impl Application for App {
     fn view(&self) -> Element<Message> {
         let is_sharing = self.capturer.is_running();
 
-        column_iced![
-            input("Display", &self.capturer.args.display.to_string(), &Message::SetDisplay),
-            input("Max FPS", &self.capturer.config.max_fps.to_string(), &Message::SetMaxFps),
-            row![
+        let a: Element<Message> = row![
+            // input("Display", &self.capturer.args.display.to_string(), Message::SetDisplay),
+            // input("Max FPS", &self.capturer.config.max_fps.to_string(), Message::SetMaxFps),
+            column_iced![
                 button(
-                    if is_sharing {
-                        "Stop"
-                    } else {
-                        "Start"
-                    },
-                ).on_press(
+                    text(
+                        if is_sharing {
+                            "Stop Sharing"
+                        } else {
+                            "Start Sharing"
+                        })
+                    .size(16),
+                ).style(
+                    iced::theme::Button::Custom(Box::new(MaterialButton::new(
+                        if is_sharing {
+                            iced::theme::Button::Destructive
+                        } else {
+                            iced::theme::Button::Primary
+                        }
+                    )))
+                )
+                .padding([10, 24, 10, 24])
+                .on_press(
                     if is_sharing {
                         Message::Stop
                     } else {
                         Message::Start
                     }
                 ),
+                if is_sharing {
+                    Element::from(
+                        button(
+                            text(
+                                "Copy invite link"
+                            ).size(16),
+                        ).style(
+                            iced::theme::Button::Custom(Box::new(MaterialButton::new(
+                                iced::theme::Button::Primary
+                            )))
+                        )
+                        .padding([10, 24, 10, 24])
+                        .on_press(Message::CopyInviteLink)
+                    )
+                } else {
+                    Element::from(row![])
+                }
             ]
+            .align_items(iced::Alignment::Center)
+            .width(iced::Length::Fill)
             .padding(10)
             .spacing(10),
         ]
+            .align_items(iced::Alignment::Center)
+            .height(iced::Length::Fill)
             .padding(10)
-            .into()
+            .into();
+        // a.explain(Color::WHITE)
+        a
+    }
+
+    fn theme(&self) -> Self::Theme {
+        Theme::Custom(
+            Box::from(iced::theme::Custom::new(
+                Palette {
+                    background: Color::from_rgb(0.2, 0.2, 0.2),
+                    primary: Color::from_rgb(0.0, 0.5, 0.5),
+                    ..Palette::DARK
+                }
+            ))
+        )
     }
 }
 
 fn input<'a>(
     name: &str,
     value: &str,
-    message: &'a dyn Fn(String) -> Message,
+    message: impl Fn(String) -> Message + 'a,
 ) -> Element<'a, Message> {
     row![
         text(format!("{}: ", name)),
         text_input(
             name,
             value,
-        ).on_input(|value| {
+        ).on_input(move |value| {
             message(value)
         })
         .width(iced::Length::Fixed(100.)),
@@ -119,4 +172,62 @@ fn input<'a>(
         .padding(10)
         .spacing(10)
         .into()
+}
+
+struct MaterialButton {
+    style: Button,
+}
+
+impl MaterialButton {
+    pub fn new(style: Button) -> Self {
+        Self {
+            style
+        }
+    }
+}
+
+impl button::StyleSheet for MaterialButton {
+    type Style = Theme;
+
+    fn active(&self, style: &Self::Style) -> Appearance {
+        let appearance = Appearance {
+            border_radius: 32.0,
+            border_width: 0.0,
+            ..Appearance::default()
+        };
+
+        let from_pair = |pair: iced::theme::palette::Pair| Appearance {
+            background: Some(pair.color.into()),
+            text_color: pair.text,
+            ..appearance
+        };
+
+        let palette = style.extended_palette();
+
+        match self.style {
+            Button::Primary => from_pair(palette.primary.strong),
+            Button::Secondary => from_pair(palette.secondary.base),
+            Button::Positive => from_pair(palette.success.base),
+            Button::Destructive => from_pair(palette.danger.base),
+            Button::Text | Button::Custom(_) => Appearance {
+                text_color: palette.background.base.text,
+                ..appearance
+            },
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style) -> Appearance {
+        let active = self.active(style);
+        Appearance {
+            background: active.background.map(|background| match background {
+                Background::Color(color) => Background::Color(Color {
+                    r: color.r - 0.1,
+                    g: color.g - 0.1,
+                    b: color.b - 0.1,
+                    a: color.a,
+                }),
+            }),
+            ..active
+        }
+    }
 }
